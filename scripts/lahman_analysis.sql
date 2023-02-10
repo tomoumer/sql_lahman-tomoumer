239c5d9bc8661d0bb2b4853682dc6aea04e06dd8
tomoumer <tomo.umer@gmail.com>
2023-02-09 21:31:30 -0600

    solved through ex 7

--- scripts/lahman_analysis.sql
+++ scripts/lahman_analysis.sql
@@ -7,14 +7,16 @@ USING(schoolid)
 WHERE schoolname='Vanderbilt University';
 */
 
--- WITH vandy_players AS (SELECT DISTINCT
+-- WITH vandy_players AS (
+-- SELECT DISTINCT
 -- 	playerid,
 -- 	namefirst,
 -- 	namelast
 -- FROM people
 -- INNER JOIN collegeplaying
 -- USING(playerid)
--- WHERE schoolid='vandy')
+-- WHERE schoolid='vandy'
+-- )
 -- SELECT
 -- 	--playerid,
 -- 	namefirst,
@@ -36,9 +38,11 @@ WHERE schoolname='Vanderbilt University';
 -- 		WHEN pos IN ('P', 'C') THEN 'Battery'
 -- 		END AS position_group,
 -- 	SUM(po) AS total_putout
+-- /* this part was superfluous!
 -- FROM people
--- LEFT JOIN fielding
--- USING(playerid)
+-- --LEFT JOIN fielding
+-- USING(playerid)*/
+-- FROM fielding
 -- WHERE yearid = 2016
 -- GROUP BY position_group;
 
@@ -52,13 +56,14 @@ WHERE schoolname='Vanderbilt University';
 -- SELECT
 -- 	begin_decade,
 -- 	end_decade,
--- 	ROUND(AVG((SO+SOA)::NUMERIC / G), 2) AS average_strikeouts
+-- 	ROUND(AVG((SO+SOA)::NUMERIC / G), 2) AS average_strikeouts,
+-- 	ROUND(AVG((HR+HRA)::NUMERIC / G), 2) AS average_homeruns
 -- FROM decades
 -- LEFT JOIN teams
 -- ON yearid >= begin_decade
 -- AND yearid <= end_decade
 -- GROUP BY begin_decade, end_decade
--- ORDER BY begin_decade
+-- ORDER BY begin_decade;
 
 -- 4. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases. Report the players' names, number of stolen bases, number of attempts, and stolen base percentage.
 -- note: attempts are stolen bases (sb) + caught stealing (cs). By groupingby playerid I also ensured that the resulting join with those limits doesn't have duplicate players (when they played for multiple teams)
@@ -116,9 +121,95 @@ WHERE schoolname='Vanderbilt University';
 -- )	
 -- SELECT 
 -- 	SUM(CASE WHEN (max_w IS NOT NULL AND wswin ='Y') THEN 1 END) AS win_win,
--- 	ROUND(100.* SUM(CASE WHEN (max_w IS NOT NULL AND wswin ='Y') THEN 1 END) /
--- 	SUM(CASE WHEN (max_w IS NOT NULL) THEN 1 END),2) AS fraction_win
+-- 	ROUND(100.* SUM(CASE WHEN (max_w IS NOT NULL AND wswin ='Y') THEN 1 END) / (2016 - 1970),2) AS fraction_win
 -- FROM teams t
 -- LEFT JOIN year_wins
 -- ON t.yearid = year_wins.yearid
 -- 	AND t.W = year_wins.max_w;
+
+-- SUM(CASE WHEN (max_w IS NOT NULL) THEN 1 END) doesnt work because there are multiple teams with max wins
+
+/* Monica's code
+WITH w_rank AS(	SELECT teamid,
+			   		   name,
+					   yearid,
+					   RANK() OVER(PARTITION BY yearid ORDER BY w DESC)
+				FROM teams
+			  	WHERE yearid >= 1970),
+	 ws_wins AS(SELECT teamid, yearid
+				FROM teams
+				WHERE wswin = 'Y'
+					AND yearid >= 1970	)
+SELECT COUNT(*) AS count_ws_max_w_teams,
+	   ROUND(COUNT(*) * 100.0 / (2016-1969), 2) AS percent_ws_max_w_teams
+FROM w_rank
+	 INNER JOIN ws_wins 
+	 USING(teamid, yearid)
+WHERE rank = 1;
+*/
+
+-- 6. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
+
+/* first attempt was getting me two columns with years aaand that was no good.
+WITH winner_managers AS (
+	SELECT
+		playerid,
+		yearid,
+		lgid
+	FROM awardsmanagers
+	WHERE awardid = 'TSN Manager of the Year'
+		AND lgid = 'NL'
+)
+SELECT
+	winner_managers.playerid,
+	winner_managers.yearid AS NL_win,
+	awardsmanagers.yearid AS AL_win,
+	teamid
+FROM winner_managers
+LEFT JOIN awardsmanagers
+USING(playerid)
+LEFT JOIN managers
+	ON awardsmanagers.playerid = managers.playerid
+	AND (winner_managers.yearid = managers.yearid OR
+		awardsmanagers.yearid = managers.yearid)
+WHERE awardid = 'TSN Manager of the Year'
+	AND awardsmanagers.lgid = 'AL';
+*/
+
+-- SELECT
+-- 	namefirst,
+-- 	namelast,
+-- 	awmg.yearid,
+-- 	awmg.lgid,
+-- 	teamid
+-- FROM awardsmanagers AS awmg
+-- LEFT JOIN people
+-- USING(playerid)
+-- LEFT JOIN managers AS m
+-- ON awmg.playerid = m.playerid
+-- 	AND awmg.yearid = m.yearid
+-- WHERE (awmg.playerid, awmg.awardid) IN (
+-- 	SELECT playerid,
+-- 		awardid
+-- 	FROM awardsmanagers
+-- 	WHERE awardid = 'TSN Manager of the Year'
+-- 		AND lgid IN ('NL','AL')
+-- 	GROUP BY playerid, awardid
+-- 	HAVING COUNT( DISTINCT lgid) = 2
+-- )
+
+-- 7. Which pitcher was the least efficient in 2016 in terms of salary / strikeouts? Only consider pitchers who started at least 10 games (across all teams). Note that pitchers often play for more than one team in a season, so be sure that you are counting all stats for each player.
+
+-- SELECT 
+-- 	playerid,
+-- 	SUM(so) AS tot_strikeouts,
+-- 	SUM(salary) AS tot_salary
+-- FROM pitching p
+-- LEFT JOIN salaries
+-- USING (playerid)
+-- WHERE p.yearid = 2016
+--  	AND salaries.yearid = 2016
+-- GROUP BY playerid
+-- HAVING SUM(g) > 10
+
+-- 8. Find all players who have had at least 3000 career hits. Report those players' names, total number of hits, and the year they were inducted into the hall of fame (If they were not inducted into the hall of fame, put a null in that column.) Note that a player being inducted into the hall of fame is indicated by a 'Y' in the **inducted** column of the halloffame table.
